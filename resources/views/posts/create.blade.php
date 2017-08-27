4cfcafa40d07420320593091b0f00df05bc80eab
Thiago Colares <thicolares@gmail.com>
2017-08-26 22:48:27 -0300

    Experimenta um POST request via form com csrf_token

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,14 +3,15 @@
 @section('content')
     <h1>Create a post</h1>
     <form method="POST" action="/posts">
+        {{ csrf_field() }}
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" aria-describedby="titleHelp">
-            <small id="titleHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <input type="text" class="form-control" id="title" aria-describedby="titleHelp" name="title">
+            <small id="titleHelp" class="form-text text-muted">Seja objetivo</small>
         </div>
         <div class="form-group">
-            <label for="body">Password</label>
-            <textarea class="form-control" id="body" rows="3"></textarea>
+            <label for="body">Boby</label>
+            <textarea class="form-control" id="body" rows="3" name="body"></textarea>
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
     </form>
