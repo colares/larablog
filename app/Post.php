8c20c40ae6cc70e931cbb41181bef9a1695afa3f
Thiago Colares <thicolares@gmail.com>
2017-08-29 22:44:49 -0300

    Cria o model Comments e testa relações com o Post

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
-//    protected $guarded = ['user_id'];
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 }
