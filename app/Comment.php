9ba6d3f66264e2b78a7631f02a40145b73b6caa8
Thiago Colares <thicolares@gmail.com>
2017-09-09 12:08:17 -0300

    Associa User, Post e Comment

--- app/Comment.php
+++ app/Comment.php
@@ -8,4 +8,12 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    // Então, para pegar o nome do criador do comentário,
+    // poderei usar essa syntaxe:
+    // $comment->user->name
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -10,6 +10,15 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    // Então, para pegar o nome do criador do comentário,
+    // poderei usar essa syntaxe:
+    // $post->user->name
+    // $comment->post->user->name
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body) {
         $this->comments()->create(compact('body'));
     }
