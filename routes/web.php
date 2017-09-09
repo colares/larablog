499b2a20cbcb5a9bcedcb58197a41d4833d98b99
Thiago Colares <thicolares@gmail.com>
2017-09-09 16:57:36 -0300

    Cria os controllers para login e criação de conta

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+
+Route::get('/register', 'RegistrationsController@create');
+Route::get('/login', 'SessionsController@create');
+
 // use App\Task;
 
 // Route::get('/tasks', 'TasksController@index');
