d3fffd9b3b348cd85e43e55e59c80e1ee67631d5
Thiago Colares <thicolares@gmail.com>
2017-09-10 15:08:33 -0300

    Filtra a listagem de posts após clique em um dos itens de Archives

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Post;
 
@@ -16,7 +17,17 @@ class PostsController extends Controller
     }
 
     public function index() {
-        $posts = Post::latest()->get();
+        $posts = Post::latest();
+
+        if ($month = request('month')) {
+            $posts->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if ($year = request('year')) {
+            $posts->whereYear('created_at', $year);
+        }
+
+        $posts = $posts->get();
 
 
         $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,8 +5,8 @@
 <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
-        @foreach($archives as $month)
-            <li><a href="">{{ $month['month'] . ' ' . $month['year'] }}</a></li>
+        @foreach($archives as $stats)
+            <li><a href="/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">{{ $stats['month'] . ' ' . $stats['year'] }}</a></li>
         @endforeach
     </ol>
 </div>
