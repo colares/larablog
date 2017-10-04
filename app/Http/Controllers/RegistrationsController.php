caec8a6aaa4c5b3b55e89a2d88e9b17fa66629a8
Thiago Colares <thicolares@gmail.com>
2017-10-03 22:50:09 -0300

    Extrai o código que submete os dados enviados pelo form

--- app/Http/Controllers/RegistrationsController.php
+++ app/Http/Controllers/RegistrationsController.php
@@ -17,27 +17,8 @@ class RegistrationsController extends Controller
 
     public function store(RegistrationRequest $request)
     {
-
-        // create and save
-//          $user = User::create(request(['name', 'email', 'password']));
-          $user = User::create([
-              'name' => request('name'),
-              'email' => request('email'),
-              'password' => bcrypt(request('password'))
-          ]);
-
-        // sign them in
-        // Auth::login($user);
-        // usando uma helper function, a gente não precisa colocar mais um use na classe:
-        auth()->login($user);
-
-
-        \Mail::to($user)->send(new Welcome($user));
-
         // redirect to the home page
         // Para ->home() funcionar, você precisa dizer nomear a rota que deseja chamar de home
         return redirect()->home();
-
-
     }
 }
--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Mail\Welcome;
+use App\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegistrationRequest extends FormRequest
@@ -28,5 +30,27 @@ class RegistrationRequest extends FormRequest
             'email' => 'required|email',
             'password' => 'required|confirmed'
         ];
+    }
+
+    public function persist()
+    {
+        // como aqui é uma requeste, posso usar $this-> em vez de request
+//          $user = User::create(request(['name', 'email', 'password']));
+        // lembrando que o código acima é igual a
+        //          $user = User::create(request()->only(['name', 'email', 'password']));
+        // então:
+        $user = User::create(
+            $this->only(['name', 'email', 'password'])
+        );
+
+
+        // sign them in
+        // Auth::login($user);
+        // usando uma helper function, a gente não precisa colocar mais um use na classe:
+        auth()->login($user);
+
+
+        \Mail::to($user)->send(new Welcome($user));
+
     }
 }
