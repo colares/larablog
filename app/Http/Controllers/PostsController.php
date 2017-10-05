3ff471982290f35c3b715b2a1445a23f5c04d86b
Thiago Colares <thicolares@gmail.com>
2017-10-05 20:35:15 -0300

    Experimenta estilo e flash message para feedback da publicação de um novo post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -81,6 +81,8 @@ class PostsController extends Controller
             new Post(request(['title', 'body']))
         );
 
+        session()->flash('message', 'Your post has now been published.');
+
         return redirect('/');
     }
 }
