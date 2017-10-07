f9d6526ad536bbf3caa13a271ecdcc39034c9e32
Thiago Colares <thicolares@gmail.com>
2017-10-07 11:06:27 -0300

    Cria o relacionamento entre Posts e Tags usando migration

--- database/migrations/2017_10_07_133637_create_tags_table.php
+++ database/migrations/2017_10_07_133637_create_tags_table.php
@@ -18,6 +18,13 @@ class CreateTagsTable extends Migration
             $table->string('name')->unique();
             $table->timestamps();
         });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->integer('post_id');
+            $table->integer('tag_id');
+            $table->timestamps();
+            $table->primary(['post_id', 'tag_id']);
+        });
     }
 
     /**
@@ -28,5 +35,6 @@ class CreateTagsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 }
